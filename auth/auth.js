98319e3567d416fed25e2e921fac83a2308e5fea
Malaka <malakasilva43@gmail.com>
2020-10-06 15:27:39 +0530

    add readme

--- auth/auth.js
+++ auth/auth.js
@@ -37,34 +37,11 @@ function storeToken(code) {
 
 function authorize() {
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) getNewToken(oAuth2Client);
+        if (err) console.log(err);
         else oAuth2Client.setCredentials(JSON.parse(token));
     });
 
     return oAuth2Client;
-}
-
-function getNewToken(oAuth2Client) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-        });
-    });
 }
 
 module.exports = {
--- public/extra/footer.js
+++ public/extra/footer.js
@@ -4,7 +4,6 @@ const Footer = () => (
   <div className="footer">
     <p className="creators">Created By: <a href="https://github.com/MalakaDeSilva">Malaka Silva</a> || <a href="https://github.com/dulajkavinda">Dulaj Kavinda</a></p>
     <p>Copyright © 2020</p>
-    <div>Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   </div>
 );
 
